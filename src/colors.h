f3246d9b6da178189f367fd0f1908825285902d3
Ben <ben>
2016-01-14 17:39:10 +0000

    Now readable on real Pebble (more contrast)

--- src/colors.h
+++ src/colors.h
@@ -1,7 +1,7 @@
 #include <pebble.h>
 
-#define COLOR_BG GColorVeryLightBlue
-#define COLOR_TEXT GColorRichBrilliantLavender
-#define COLOR_HIGHLIGHT GColorRichBrilliantLavender
+#define COLOR_BG GColorBlue
+#define COLOR_TEXT GColorWhite
+#define COLOR_HIGHLIGHT GColorPurple
 #define COLOR_HIGHLIGHT_TEXT GColorWhite
-#define WRONG_ANSWER_COLOR GColorElectricUltramarine
+#define WRONG_ANSWER_COLOR GColorDarkCandyAppleRed
